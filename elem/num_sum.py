c9c4b815fff2349d060d0604f33f475e1aa1645d
Uthaeus <romanlavery@gmail.com>
2020-11-01 21:03:19 -0700

    completed num_sum.py

--- elem/num_sum.py
+++ elem/num_sum.py
@@ -1,8 +1,16 @@
 
 
 def num_sum(num):
-    r = range(1, num)
-    print(r)
+    r = range(1, num + 1)
+    result = reduce((lambda a, b: a + b), r)
+    return result 
 
 
-print(num_sum(5))
+def hey_there():
+    print('Enter a number between 1 and 99')
+    num = raw_input()
+    summed = num_sum(int(num))
+    print('Your sum is {}'.format(summed))
+
+
+hey_there()
