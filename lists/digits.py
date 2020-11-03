c7f5d6fda17936ce31d99d3f41b1faa9e7e17fc1
Uthaeus <romanlavery@gmail.com>
2020-11-03 08:47:07 -0700

    refactored digits.py

--- lists/digits.py
+++ lists/digits.py
@@ -1,9 +1,10 @@
 
 
 def digify(num):
-    s = str(num)
+    # s = str(num)
     
-    l = map(lambda x: int(x), s)
-    return l 
+    # l = map(lambda x: int(x), s)
+    # return l 
+    return map(lambda x: int(x), str(num))
 
 print(digify(4321))
